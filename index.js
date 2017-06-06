c9006fab8564afca4ea9b9b5912d1073db8ba10d
Jake Musich <makejusich@gmail.com>
2017-06-06 17:04:20 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+  var counter = 0
+
+  function addBump() {
+    counter += 1
+  }
+
+  function getBumps(){
+    return counter
+  }
+
+  return {
+    addBump,
+    getBumps
+  };
+}
+
+function createAnimal(animalType) {
+  return function armAnimal(deadlyDevice) {
+    var newAnimal = {animalType: '${animalType}', deadlyDevice: '${deadlyDevice}'}
+  }
+}
+
+var sharkCreator = (a) => createAnimal['shark'] = a
+var sharkWithFrickinLaserbeam = {animalType: 'Shark', deadlyDevice: 'Laserbeam'}
+var sharkWithFrickinCannon = {animalType: 'Shark', deadlyDevice: 'Cannon'}
